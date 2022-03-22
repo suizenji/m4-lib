a51ea58f49bfb7919fc04eadb49808130c98fbca
suizenji <suizenji@pop-os.localdomain>
2022-03-22 20:29:24 +0900

    fix-lambda-make

--- c-lambda/make.sh
+++ c-lambda/make.sh
@@ -3,16 +3,18 @@
 SEP=${SEP1/\\/}${SEP2/\\/}
 
 cat <<EOF
-include(${DIR_LIB}/util.m4)dnl
-include(${DIR_LIB}/lambda.m4)dnl
+${PREF}changequote(,)${PREF}dnl
+${PREF}changequote($SEP1,$SEP2)${PREF}dnl
+${PREF}include(${DIR_LIB}/util.m4)${PREF}dnl
+${PREF}include(${DIR_LIB}/lambda.m4)${PREF}dnl
 #include <stdio.h>
 
 // __LAMDEF__
 
 int main(void) {
-    int (*l0)() = _(int , return 0;);
-    char *l1 = _(char *, return "str";)${SEP}();
-    int l2l3 = _(int , int i, int j, return i * j;)${SEP}(2, _(int , int k, return k;)${SEP}(3));
+    int (*l0)() = ${PREF}_(int , return 0;);
+    char *l1 = ${PREF}_(char *, return "str";)${SEP}();
+    int l2l3 = ${PREF}_(int , int i, int j, return i * j;)${SEP}(2, ${PREF}_(int , int k, return k;)${SEP}(3));
 
     return 0;
 }
